bb5b1161f54d6e69e3c21b1ac233b0d2d81ad924
mathewqpmiller <mathewpqmiller@gmail.com>
2021-04-10 20:56:19 -0700

    Guage Plot Parameters

--- static/js/test_app.js
+++ static/js/test_app.js
@@ -1,15 +1,17 @@
-// App to create the dropdown menu with id's, bar plot, guage plot and bubble plot
-
-// Create function to read in all .json data
-function jsonInfo(id){
+//creating function for all the demographic info
+function demoInfo(id){
     d3.json("samples.json").then((data)=> {
         var metadata = data.metadata;
         console.log(metadata);
+
         //filter demo info data by id
         var filterResult = metadata.filter(info => info.id.toString() === id)[0];
+
         var panelBody = d3.select("#sample-metadata");
+
         //empty the demo info panel each time before getting new data
         panelBody.html("");
+
         Object.entries(filterResult).forEach((key)=>{
             panelBody.append("p").text(key[0] + ":" + key[1]);
         });
@@ -21,34 +23,34 @@ function plots(id) {
     // getting data from the json file
     d3.json("samples.json").then((data)=> {
         //console.log(data)
-
-        // Create a variable that holds the washed frequency
+        
+        //filter wfreq value by id
         var wfreq = data.metadata.filter(f => f.id.toString() === id)[0];
         wfreq = wfreq.wfreq;
         console.log("Washing Freq: " + wfreq);
-
-        // filter samples+ values by id
+        
+        // filter samples+ values by id 
         var samples = data.samples.filter(s => s.id.toString() === id)[0];
-
+        
         //console.log("Samples: " + samples);
-
-        // Getting the top 10
+  
+        // Getting the top 10 
         var samplevalues = samples.sample_values.slice(0, 10).reverse();
         console.log("top 10 sample: " + samplevalues);
-
-        // get only top 10 otu ids for the plot OTU and reversing it.
+  
+        // get only top 10 otu ids for the plot OTU and reversing it. 
         var OTU = (samples.otu_ids.slice(0, 10)).reverse();
-
+        
         // get the otu id's to the desired form for the plot
         var OTU_id = OTU.map(d => "OTU " + d)
-
+  
         console.log("OTU IDS: " + OTU_id);
-
-
+  
+  
         // get the top 10 labels for the plot and reversing it.
         var labels = samples.otu_labels.slice(0, 10).reverse();
         console.log("labels: " + labels);
-
+  
         // create trace variable for the plot
         var trace = {
             x: samplevalues,
@@ -59,15 +61,15 @@ function plots(id) {
             type:"bar",
             orientation: "h",
         };
-
+  
         // create data variable
         var data = [trace];
-
+  
         // create layout variable to set plots layout
-
+  
         // create the bar plot
         Plotly.newPlot("bar", data);
-
+      
         // The bubble chart
         var trace1 = {
             x: samples.otu_ids,
@@ -78,55 +80,54 @@ function plots(id) {
                 color: samples.otu_ids
             },
             text: samples.otu_labels
-
+  
         };
-
+  
         // set the layout for the bubble plot
         var layout_b = {
             xaxis:{title: "OTU ID"},
             height: 600,
             width: 1200
         };
-
-        // creating data variable
+  
+        // creating data variable 
         var data1 = [trace1];
-
+  
         // create the bubble plot
-        Plotly.newPlot("bubble", data1, layout_b);
-
-        // Create the guage plot paramaters
-        var guage_plot = [
+        Plotly.newPlot("bubble", data1, layout_b); 
+  
+        // The guage chart
+        var data_g = [
           {
           domain: { x: [0, 1], y: [0, 1] },
           value: wfreq,
-          title: {text: `Times Washed Per Week`},
+          title: {text: `Belly Button Washing Frequency`},
           type: "indicator",
-          delta: {reference: 9, 'increasing': {color: "red"}},
-          mode: "gauge+number+delta",
+          
+          mode: "gauge+number",
           gauge: { axis: { range: [null, 9] },
-                  bar: {color: "68b1b6"},
                    steps: [
-                    {range: [0, 1], color: "f4f4f6"},
-                    {range: [1, 2], color: "eae9ec"},
-                    {range: [2, 3], color: "dfdee3"},
-                    {range: [3, 4], color: "d5d3d9"},
-                    {range: [4, 5], color: "cac8d0"},
-                    {range: [5, 6], color: "c0bec6"},
-                    {range: [6, 7], color: "b5b3bd"},
-                    {range: [7, 8], color: "aaa8b3"},
-                    {range: [8, 9], color: "a09daa"}
+                    {range: [0, 1], color: "white"},
+                    {range: [1, 2], color: "white"},
+                    {range: [2, 3], color: "white"},
+                    {range: [3, 4], color: "white"},
+                    {range: [4, 5], color: "white"},
+                    {range: [5, 6], color: "white"},
+                    {range: [6, 7], color: "white"},
+                    {range: [7, 8], color: "white"},
+                    {range: [8, 9], color: "green"}
                   ]}
-
+              
           }
         ];
-        var layout_g = {
-            width: 600,
-            height: 600,
-            margin: { t: 20, b: 40, l:100, r:100 }
+        var layout_g = { 
+            width: 500, 
+            height: 600, 
+            margin: { t: 20, b: 40, l:100, r:100 } 
           };
-        Plotly.newPlot("gauge", guage_plot, layout_g);
+        Plotly.newPlot("gauge", data_g, layout_g);
       });
-  }
+  }  
 
 function init() {
     //read the data
@@ -141,7 +142,7 @@ function init() {
             .property("value");
         });
         plots(data.names[0]);
-        jsonInfo(data.names[0]);
+        demoInfo(data.names[0]);
     });
 };
 init();
@@ -149,5 +150,5 @@ init();
 //change event function
 function optionChanged(id){
     plots(id);
-    jsonInfo(id);
+    demoInfo(id);
 };
